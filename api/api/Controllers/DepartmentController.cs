676f2ba47e16553fcee7f2a7c08141a83d755a19
Dumota <edumota02@gmail.com>
2022-08-16 20:44:59 -0300

    first commit

---
+++ api/api/Controllers/DepartmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
